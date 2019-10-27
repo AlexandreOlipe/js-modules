8757490e64eaec350fd90a440cbe6c86341c58fd
Alexandre de Oliveira Pereira <alexandre.olipe@gmail.com>
2019-10-27 10:12:54 -0300

    Added webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        filename: './src/app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: './bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+}
